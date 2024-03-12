4bce484853456e3791a8a1b79c20b3deedc0c84a
Kevinoscal <145907554+Kevinoscal@users.noreply.github.com>
2024-03-11 20:37:04 -0600

    Add files via upload

---
+++ ExamenEjercicio2/src/com/kl/examen2/Ejercicio2.java
@@ -0,0 +1,12 @@
+package com.kl.examen2;
+
+public class Ejercicio2 {
+	public void juntarNombre(String cadena) {
+		String[] partes = cadena.split("");
+		
+		String primerNombre = partes[0];
+		String SegundoNombre = partes[2];
+		
+		System.out.print(primerNombre + "" + primerApellido);
+	}
+}
---
+++ ExamenEjercicio2/src/com/kl/test/main.java
@@ -0,0 +1,14 @@
+package com.kl.test;
+
+import com.kl.examen2.Arreglos;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Arreglos arreglo = new Arreglos ();
+		arreglo.juntarNombre("KevinLopez");
+		}
+
+}
